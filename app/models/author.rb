3733e943396809c00c95dad70fbc93dd0f52df59
Liz Burton <liz.burton147@gmail.com>
2021-05-26 14:40:30 -0400

    add associations to starter code

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  has_one :profile
+  has_many :posts
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
