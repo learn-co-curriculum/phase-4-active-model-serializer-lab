ac18c41cc435e0725e20dfce21d71c283532c62a
Liz Burton <liz.burton147@gmail.com>
2021-05-26 14:40:57 -0400

    remove post_tag resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :tags
-  resources :profiles
-  resources :posts
-  resources :post_tags
-  resources :authors
+  resources :tags, only: [:index, :show]
+  resources :profiles, only: [:index, :show]
+  resources :posts, only: [:index, :show]
+  resources :authors, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
