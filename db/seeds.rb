5918fcd0741a247e59b4e1f9300d8e773cc9bb26
Liz Burton <liz.burton147@gmail.com>
2021-05-27 07:16:34 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 3.times do
   author = Author.create(name: Faker::Name.unique.name)
-  author.create_profile(email: Faker::Internet.email, username: Faker::Internet.username, bio: Faker::Lorem.paragraph, avatar_url: Faker::Avatar.image)
+  author.create_profile(email: Faker::Internet.email, username: Faker::Internet.username, bio: Faker::Hipster.paragraph, avatar_url: Faker::Avatar.image)
   num_times = rand(1..5)
   num_times.times do
-    author.posts.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+    author.posts.create(title: Faker::Book.title, content: Faker::Hipster.paragraph(sentence_count: 5, random_sentences_to_add: 4))
   end
 end
 
@@ -13,7 +13,9 @@ end
 
 Post.all.each do |post|
   num_times = rand(1..5)
+  tags = Tag.all.to_a
+
   num_times.times do
-    post.tags.append(Tag.find(Tag.pluck(:id).sample))
+    post.tags.append(tags.shuffle!.pop)
   end
 end 
